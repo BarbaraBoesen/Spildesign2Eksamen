bdac6ea47fb6ab9d1e5969406b5086e873d12079
leflander <127100665+leflander@users.noreply.github.com>
2023-03-19 16:43:53 +0100

    interactions script
    basic interaction script

--- My project (4)/Assets/SignTestPopUp.cs
+++ My project (4)/Assets/SignTestPopUp.cs
@@ -3,20 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SignTestPopUp : MonoBehaviour
+    
 {
-    Rigidbody2D rb;
-    // Start is called before the first frame update
-    void Start()
+    public bool ReadingDistance;
+
+// Start is called before the first frame update
+void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        ReadingDistance = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && ReadingDistance == true)
         {
-            Debug.Log("e key was pressed");
+            Debug.Log("Smith Estate");
         }
+       
+    }
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        ReadingDistance = true;
+        Debug.Log("Press E");
+    }
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        ReadingDistance = false;
     }
 }
